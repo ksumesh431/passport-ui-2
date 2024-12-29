ce8d12b247afab2281ac5c2052a914398596d793
Sumesh Kumar Panda <sumeshpanda.k@gmail.com>
2024-12-29 22:41:35 +0530

    fixed carasoul and slider

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,19 +1,23 @@
-import { CarouselComponent } from '@/components/carasoul'
-import SkillStoneFeatures from '@/components/slider'
-import {ContactForm, ExpertServices} from '@/components/contactForm'
-import BlogPosts from '@/components/blogposts'
+import { CarouselComponent } from '@/components/carasoul';
+import SkillStoneFeatures from '@/components/slider';
+import { ContactForm, ExpertServices } from '@/components/contactForm';
+import BlogPosts from '@/components/blogposts';
+import EmblaCarousel from '@/components/EmblaCarousel';
+import MultiSlideCarousel from '@/components/MultiSlideCarousel';
 const Dashboard = () => {
   return (
     <>
-      <CarouselComponent />
-      {/* <div className="relative w-full max-w-screen-lg mx-auto"> */}
-        <SkillStoneFeatures />
-      {/* </div> */}
+      <div className="mt-5">
+        <EmblaCarousel />
+      </div>
+      <div className="mt-10">
+        <MultiSlideCarousel />
+      </div>
       <ExpertServices />
       <ContactForm />
       <BlogPosts />
     </>
-  )
-}
+  );
+};
 
-export default Dashboard
+export default Dashboard;
